b0521a0c2a0c7465da5b385430bf4c1144d4b666
Jasleen-001 <kaurjasleen1103@gmail.com>
2022-09-23 02:00:04 +0530

    "bttom navigation menu added"

--- app/src/main/java/com/jasleen/clocko_meter/MainActivity.java
+++ app/src/main/java/com/jasleen/clocko_meter/MainActivity.java
@@ -1,14 +1,37 @@
 package com.jasleen.clocko_meter;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
-
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+       // NavigationBarView.OnItemSelectedListener(this);
+
+        bottomNavigationView.setSelectedItemId(R.id.timer);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                return true;
+            }
+
+        });
     }
+    FragmentFirst firstFragment = new FragmentFirst();
+    FragmentSecond secondFragment = new FragmentSecond();
+    FragmentThird thirdFragment = new FragmentThird();
+
 }
